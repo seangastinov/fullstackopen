5f933b5b535061d27080aae5ae1daa15a33448a6
seangastinov <seangastinov@gmail.com>
2023-07-27 23:45:49 +0700

    part 1.17-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,47 @@
 import { useState } from 'react'
 
+const Statistics = ({good,neutral,bad}) => {
+    const all = bad+good+neutral
+    const avg = (good+bad*-1)/all
+    const pos = good/all*100
+    if (all === 0) {
+        return (
+            <div>
+                <h1>statistics</h1>
+                No feedback given
+            </div>
+        )
+    }
+    return(
+        <div>
+            <h1>statistics</h1>
+            <table>
+                <tbody>
+                <StatisticLine name={'good'} state={good}/>
+                <StatisticLine name={'neutral'} state={neutral}/>
+                <StatisticLine name={'bad'} state={bad}/>
+                <StatisticLine name={'all'} state={all}/>
+                <StatisticLine name={'average'} state={avg}/>
+                <StatisticLine name={'positive'} state={pos} add={'%'}/>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+const Button = ({handler,name, state, setter}) => {
+    return(
+        <button onClick={() => handler(name,state,setter)}>{name}</button>
+    )
+}
 
+const StatisticLine = ({name, state, add}) => {
+    return(
+        <tr>
+            <td>{name}</td>
+            <td>{state} {add}</td>
+        </tr>
+    )
+}
 const App = () => {
     const handler = (name,state,setter) =>{
         const updatedState = state+1
@@ -11,20 +52,15 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
-        return (
-          <div>
-              <h1>give feedback</h1>
-              <button onClick={() => handler('good',good,setGood)}>good</button>
-              <button onClick={() => handler('neutral', neutral, setNeutral)}>neutral</button>
-              <button onClick={() => handler('bad',bad,setBad)}>bad</button>
-              <h1>statistics</h1>
-              <>good {good}</>
-              <br/>
-              <> neutral {neutral}</>
-              <br/>
-              <>bad {bad}</>
-          </div>
-      )
-    }
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <Button name={'good'} setter={setGood} handler={handler} state={good}/>
+            <Button name={'neutral'} setter={setNeutral} handler={handler} state={neutral}/>
+            <Button name={'bad'} setter={setBad} handler={handler} state={bad}/>
+            <Statistics  good={good} bad={bad} neutral={neutral} />
+        </div>
+    )
+}
 
 export default App
