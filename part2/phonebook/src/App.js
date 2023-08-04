76f7a52c79b94e3365e4bd6555ba8d1aa53cd1b1
seangastinov <seangastinov@gmail.com>
2023-08-04 18:17:08 +0700

    part 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [newFilter, setNewFilter] = useState('')
+
+    useEffect(() => {
+        console.log('effect function is called')
+        const eventHandler = response => {
+            console.log('promise fulfilled')
+            setPersons(response.data)
+        }
+        axios.get('http://localhost:3001/persons').then(eventHandler)
+    }, [])
+
+    console.log('phonebook has', persons.length, 'person')
 
 
     const addHandler = (event)=> {
@@ -64,7 +71,6 @@ const App = () => {
     }
 
     return (
-
             <div>
                 <h2>Phonebook</h2>
                 <Filter handler={inputFilterChangeHandler} state={newFilter}/>
