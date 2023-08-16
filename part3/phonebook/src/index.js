e6cdf27dc0f86b99cb36c6b733f949ec4503ca29
seangastinov <seangastinov@gmail.com>
2023-08-16 14:45:51 +0800

    part 3.1

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -1,6 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+const express = require('express')
+const app = express()
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let persons =[
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
